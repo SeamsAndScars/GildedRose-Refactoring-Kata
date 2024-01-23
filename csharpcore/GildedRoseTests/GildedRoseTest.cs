b7a5c9b608c2ae5f7a8ae1ff15da352e228ec80f
SeamsAndScars <102183435+SeamsAndScars@users.noreply.github.com>
2024-01-23 12:09:18 +0100

    Added tests for specific items
    Test cases for Aged Brie and Sulfuras

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -36,6 +36,46 @@ namespace GildedRoseTests
             Assert.AreEqual(0, items[0].Quality);
         }
 
-        
+        // “Aged Brie” actually increases in Quality the older it gets
+        [Test]
+        public void UpdateQuality_AgedBrie_IncreasesQualityAsItGetsOlder()
+        {
+            var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 10 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("Aged Brie", items[0].Name);
+            Assert.AreEqual(4, items[0].SellIn);
+            Assert.AreEqual(11, items[0].Quality);
+        }
+
+        // The Quality of an item is never more than 50
+        [Test]
+        public void UpdateQuality_AgedBrie_QualityNeverMoreThan50()
+        {
+            var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 50 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("Aged Brie", items[0].Name);
+            Assert.AreEqual(4, items[0].SellIn);
+            Assert.AreEqual(50, items[0].Quality);
+        }
+
+        // “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+        [Test]
+        public void UpdateQuality_Sulfuras_LegendaryItemNeverChanges()
+        {
+            var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 5, Quality = 80 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("Sulfuras, Hand of Ragnaros", items[0].Name);
+            Assert.AreEqual(5, items[0].SellIn);
+            Assert.AreEqual(80, items[0].Quality);
+        }
     }
 }
