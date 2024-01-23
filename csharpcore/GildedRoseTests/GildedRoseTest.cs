6bd5d210041549d541c29a134e3f4950597b61a4
SeamsAndScars <102183435+SeamsAndScars@users.noreply.github.com>
2024-01-23 12:07:28 +0100

    Added general tests
    Tests for degradation and quality

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -2,16 +2,40 @@
 using GildedRoseKata;
 using NUnit.Framework;
 
-namespace GildedRoseTests;
 
-public class GildedRoseTest
+namespace GildedRoseTests
 {
-    [Test]
-    public void Foo()
+    [TestFixture]
+    public class GildedRoseTest
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        var app = new GildedRose(items);
-        app.UpdateQuality();
-        Assert.AreEqual("fixme", items[0].Name);
+        // Once the sell by date has passed, Quality degrades twice as fast
+        [Test]
+        public void UpdateQuality_PastSellByDate_DegradesTwiceAsFast()
+        {
+            var items = new List<Item> { new Item { Name = "Normal Item", SellIn = 0, Quality = 10 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("Normal Item", items[0].Name);
+            Assert.AreEqual(-1, items[0].SellIn);
+            Assert.AreEqual(8, items[0].Quality);
+        }
+
+        // The Quality of an item is never negative
+        [Test]
+        public void UpdateQuality_QualityNeverNegative()
+        {
+            var items = new List<Item> { new Item { Name = "Normal Item", SellIn = 5, Quality = 0 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("Normal Item", items[0].Name);
+            Assert.AreEqual(4, items[0].SellIn);
+            Assert.AreEqual(0, items[0].Quality);
+        }
+
+        
     }
 }
